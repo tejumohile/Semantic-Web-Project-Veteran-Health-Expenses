d8d942faad620651a611870bd865e0f1ec4c1862
Tejashree Mohile <tejumohile@gmail.com>
2014-04-11 12:38:10 -0500

    Changing to Web Project.
    Created a new web Project.

---
+++ VHS/src/Data1150.java
@@ -0,0 +1,92 @@
+
+public class Data1150 {
+	public String state = null;
+	public String county = null;
+	public int fips = 0;
+	public double vPopulation = 0;;
+	public double totalExpenses = 0;
+	public double compensation = 0; 
+	public double construction = 0; 
+	public double educationVocationalRehabilitation =0;
+	public double loanGuaranty = 0;
+	public double generalOperatingExpenses = 0;
+	public double insuranceIndemnities = 0;
+	public double medicalCare = 0;
+	
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCounty() {
+		return county;
+	}
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	public int getFips() {
+		return fips;
+	}
+	public void setFips(int fips) {
+		this.fips = fips;
+	}
+	public double getvPopulation() {
+		return vPopulation;
+	}
+	public void setvPopulation(double vPopulation) {
+		this.vPopulation = vPopulation;
+	}
+	public double getTotalExpenses() {
+		return totalExpenses;
+	}
+	public void setTotalExpenses(double totalExpenses) {
+		this.totalExpenses = totalExpenses;
+	}
+	public double getCompensation() {
+		return compensation;
+	}
+	public void setCompensation(double compensation) {
+		this.compensation = compensation;
+	}
+	public double getConstruction() {
+		return construction;
+	}
+	public void setConstruction(double construction) {
+		this.construction = construction;
+	}
+	public double getEducationVocationalRehabilitation() {
+		return educationVocationalRehabilitation;
+	}
+	public void setEducationVocationalRehabilitation(
+			double educationVocationalRehabilitation) {
+		this.educationVocationalRehabilitation = educationVocationalRehabilitation;
+	}
+	public double getLoanGuaranty() {
+		return loanGuaranty;
+	}
+	public void setLoanGuaranty(double loanGuaranty) {
+		this.loanGuaranty = loanGuaranty;
+	}
+	public double getGeneralOperatingExpenses() {
+		return generalOperatingExpenses;
+	}
+	public void setGeneralOperatingExpenses(double generalOperatingExpenses) {
+		this.generalOperatingExpenses = generalOperatingExpenses;
+	}
+	public double getInsuranceIndemnities() {
+		return insuranceIndemnities;
+	}
+	public void setInsuranceIndemnities(double insuranceIndemnities) {
+		this.insuranceIndemnities = insuranceIndemnities;
+	}
+	public double getMedicalCare() {
+		return medicalCare;
+	}
+	public void setMedicalCare(double medicalCare) {
+		this.medicalCare = medicalCare;
+	}
+
+
+	
+}
